a38cb25a5d60ea169e08822265df60d70174cb2c
Soumya Ranjan Sahoo <luckyssoumya719@gmail.com>
2026-05-18 00:24:19 +0530

    updated HomePage-About's infoCard width 100% for screen size less than 1200px

--- src/Components/Home-page/Header.jsx
+++ src/Components/Home-page/Header.jsx
@@ -5,6 +5,14 @@ export const Header = () => {
     <div className={styles.header}>
         <h1>Soumya Ranjan Sahoo</h1>
         <h4>Data Engineer | MERN Stack Developer</h4>
+        <span style={{
+          fontSize: "12px",
+          fontWeight: "800",
+          color:"red",
+          position:"absolute",
+          right: "10px",
+          bottom: "5px"
+        }}>Note* - Currently projects are for demo purpose</span>
     </div>
   )
 }
